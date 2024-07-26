8910d178bf981405022f59d04a382520b4ad5b46
José Aldemir <josealdemirbrito@gmail.com>
2024-07-25 21:53:14 -0300

    update

--- Projetos-REACT/MINI-BLOG/miniblog/src/hooks/useInsertDocument.js
+++ Projetos-REACT/MINI-BLOG/miniblog/src/hooks/useInsertDocument.js
@@ -5,63 +5,62 @@ import { db } from "../firebase/config";
 import { collection, addDoc, Timestamp } from "firebase/firestore";
 
 const initialState = {
-    loading: null,
-    error: null
-}
+  loading: null,
+  error: null,
+};
 
-const insertReaducer = (state, action) =>{
-    switch(action.type) {
-        case "LOADING":
-            return {loading: true, error: null}
-        case "INSERTED_DOC":
-            return {loading: false, error: null}
-        case "ERROR":
-            return {loading: false, error: action.payload}
-        default:
-            return state
-    }
-}
+const insertReaducer = (state, action) => {
+  switch (action.type) {
+    case "LOADING":
+      return { loading: true, error: null };
+    case "INSERTED_DOC":
+      return { loading: false, error: null };
+    case "ERROR":
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 export const useInsertDocument = (docCollection) => {
-    const [response, dispacth] = useReducer(insertReaducer, initialState)
+  const [response, dispacth] = useReducer(insertReaducer, initialState);
 
-    // deal with memory leak
-    const [cancelled, setCancelled] = useState(false)
+  // deal with memory leak
+  const [cancelled, setCancelled] = useState(false);
 
-    const checkCancelBeforeDispatch = (action) => {
-        if(!cancelled){
-            dispacth(action)
-        }
+  const checkCancelBeforeDispatch = (action) => {
+    if (!cancelled) {
+      dispacth(action);
     }
+  };
 
+  const insertDocument = async (document) => {
+    checkCancelBeforeDispatch({
+      type: "LOADING",
+    });
+    try {
+      const newDocument = { ...document, createdAt: Timestamp.now() };
 
-    const insertDocument = async (document) => {
-        checkCancelBeforeDispatch({
-            type: "LOADING",
-        })
-        try {
-            const newDocument = {...document, createdAt: Timestamp.now()}
+      const insertDocument = await addDoc(
+        collection(db, docCollection),
+        newDocument
+      );
 
-            const insertDocument = await addDoc(
-                collection(db, docCollection),
-                newDocument
-            )
-
-            checkCancelBeforeDispatch({
-                type: "INSERT_DOC",
-                payload: insertDocument
-            })
-        } catch (error) {
-            checkCancelBeforeDispatch({
-                type: "ERROR",
-                payload: error.message,
-            })
-        }
+      checkCancelBeforeDispatch({
+        type: "INSERT_DOC",
+        payload: insertDocument,
+      });
+    } catch (error) {
+      checkCancelBeforeDispatch({
+        type: "ERROR",
+        payload: error.message,
+      });
     }
+  };
 
-    useEffect(() => {
-        return () => setCancelled(true)
-    }, [insertDocument])
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, [insertDocument]);
 
-    return {insertDocument, response, }
-}
+  return { insertDocument, response };
+};
--- Projetos-REACT/todo_react_ts/src/components/Modal.tsx
+++ Projetos-REACT/todo_react_ts/src/components/Modal.tsx
@@ -1,16 +1,15 @@
-import React, { Children } from "react";
-import styles from "./Modal.module.css"
+import React from "react";
+import styles from "./Modal.module.css";
 
 interface Props {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
-const Modal = ({children}: Props) => {
-
-    const closeModal = (e: React.MouseEvent) : void => {
-        const modal = document.querySelector("#modal")
-        modal?.classList.add("hide")
-    }
+const Modal = ({ children }: Props) => {
+  const closeModal = (): void => {
+    const modal = document.querySelector("#modal");
+    modal?.classList.add("hide");
+  };
 
   return (
     <div id="modal" className="hide">
