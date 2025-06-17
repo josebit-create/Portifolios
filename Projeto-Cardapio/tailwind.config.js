39bc6d4948e676ee72fce118b3d44a3f9ab7c32b
José Aldemir <josealdemirbrito@gmail.com>
2025-06-17 13:43:05 -0300

    Projeto-DevBurger: Atualizando a fonte do projeto.

--- Projeto-Cardapio/tailwind.config.js
+++ Projeto-Cardapio/tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
     extend: {
       backgroundImage: {
         home: "url('../assets/imagem-de-fundo.png')",
