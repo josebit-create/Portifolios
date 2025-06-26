65baf50d3adac573ef4af901ccad351b5adb7e0c
José Aldemir <josealdemirbrito@gmail.com>
2025-06-26 20:35:59 -0300

    Projeto DevBurger: Adicionado funcionalidades de validação do input para endereço.

--- Projeto-Cardapio/script.js
+++ Projeto-Cardapio/script.js
@@ -105,3 +105,22 @@ function removeItemCart(name) {
     updateCartModal();
   }
 }
+
+addressInput.addEventListener("input", (e) => {
+  let inputValue = e.target.value;
+
+  if (inputValue !== "") {
+    addressInput.classList.remove("border-red-500");
+    addressWarn.classList.add("hidden");
+  }
+});
+
+checkoutBtn.addEventListener("click", () => {
+  if (cart.length === 0) return;
+
+  if (addressInput.value === "") {
+    addressWarn.classList.remove("hidden");
+    addressInput.classList.add("border-red-500");
+    return;
+  }
+});
